246bec4da9c09baeac191459240e62a76025bb39
nichoth <nichoth@gmail.com>
2019-07-14 21:37:54 -0700

    foo route

---
+++ src/view/routes/foo.js
@@ -0,0 +1,27 @@
+var { h } = require('preact')
+var evs = require('../../EVENTS')
+
+function Foo (match) {
+    return function foo (props) {
+        return <div>
+            home route... foo {props.foo}
+
+            <form onSubmit={props.emit(evs.hello.world)}>
+                <input type="file" name="file"
+                    onChange={props.emit(evs.chooseFile.choose)} />
+            </form>
+
+            <div>
+                <pre>
+                    <code>
+                        {JSON.stringify(props, null, 2)}
+                    </code>
+                </pre>
+            </div>
+        </div>
+    }
+}
+
+module.exports = Foo
+
+
