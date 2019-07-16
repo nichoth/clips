94a64e682a29dcdeae244799624ea94956c486e8
nichoth <nichoth@gmail.com>
2019-07-15 20:10:23 -0700

    drag drop

--- src/view/index.js
+++ src/view/index.js
@@ -1,5 +1,6 @@
 var { h } = require('preact')
 var router = require('../router')
+var dragDrop = require('drag-drop')
 
 function App (props) {
     if (process.env.NODE_ENV === 'development') {
@@ -14,6 +15,11 @@ function App (props) {
     var routeState = props.route.pathname ?
         router.selectState(props, props.route.pathname) :
         null
+
+    dragDrop('body', function (file) {
+        console.log('drop', arguments)
+        emit('drop', file)
+    })
 
     return <div>
         <div className="app-content">
