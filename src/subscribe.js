580b42eb129d9cc0cf32f55d6817ac9709d0fd4c
nichoth <nichoth@gmail.com>
2019-08-24 17:08:19 -0700

    magically works now

--- src/subscribe.js
+++ src/subscribe.js
@@ -43,22 +43,23 @@ function Effects ({ state, view }) {
 
     function download (magnetURI, cb) {
         client.add(magnetURI, function (torrent) {
-            console.log('here', magnetURI)
-            emmit(evs.download.start, torrent)
             console.log('download', torrent)
             cb(null, torrent)
         })
     }
+    
+    var link = 'magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10&dn=Sintel&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel.torrent'
 
     // listen for DOM events
     view.on(evs.hello.world, effects.foo)
     view.on(evs.file.choose, effects.chooseFile)
     view.on(evs.file.drop, effects.onDrop)
     view.on(evs.download.start, function (ev) {
-
-        console.log('download', ev)
+        ev.preventDefault()
+        download(link, function (err, torrent) {
+            console.log('here', torrent)
+        })
     })
-
 
     return effects
 }
