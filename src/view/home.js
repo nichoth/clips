ae7e70388419c331fcc65105d74b973e31685832
nichoth <nichoth@gmail.com>
2019-08-29 14:30:47 -0700

    move things around a little

--- src/view/home.js
+++ src/view/home.js
@@ -1,14 +1,19 @@
 var { h } = require('preact')
+var path = require('path')
 var evs = require('../EVENTS')
+var extname = require('path').extname
+var concat = require('concat-stream')
 
 function Home (match) {
     return function HomeView (props) {
         console.log('props', props)
         return <div>
             <form>
-                <input type="file" name="file"
+                <input type="fie" name="file"
                     onChange={props.emit(evs.file.choose)} />
             </form>
+
+        {console.log('seed', props.files.seeding[0])}
 
         <div className="seed-videos">sharing:</div>
             {props.files.seeding[0] ?
@@ -23,6 +28,7 @@ function Home (match) {
                         </li>
                 </ul> :
                 <div className="source-videos"><em>none</em></div>
+
             }
 
         <form onSubmit={props.emit(evs.download.start)}>
@@ -33,16 +39,19 @@ function Home (match) {
         </form>
 
            <div className='download-videos'>downloading:</div>
+            {console.log('downloading', props.files.downloading[0])}
+
             {props.files.downloading[0] ?
                 <ul className="source-videos">
-                    <li className="video">
-                        <video controls>
-                            <source src={URL.createObjectURL(props.files.downloading[0])} type="video/mp4"></source>
-                        </video>
-                        <div>
-                            {props.files.seeding[0].name}
-                        </div>
+                    {props.files.downloading.map(function (torrent) {
+                        console.log('torrent', torrent)
+                        return <li className="video">
+                            <video controls>
+                                <source src={URL.createObjectURL(props.files.downloading[0].files[5])} type="video/mp4"></source>
+                            </video>
+                            <div>{torrent.name}</div>
                         </li>
+                    })}
                 </ul> :
                 <div className="source-videos"><em>none</em></div>
             }
